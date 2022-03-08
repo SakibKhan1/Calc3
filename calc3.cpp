36ed24dc38b968524b3adc96de2a3b62ec14a701
Sakib Khan <37256061+SakibKhan1@users.noreply.github.com>
2022-03-07 20:16:42 -0500

    Create calc3.cpp

---
+++ calc3.cpp
@@ -0,0 +1,43 @@
+/*
+Author: Sakib Khan
+Course: CSCI-135
+Instructor: Tong Yi
+Assignment: Project 1D
+Writes a calculator that can
+understand squared numbers. 
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int sum = 0;
+    int next;
+    char op;
+    cin >> sum;
+    if (cin.peek() == '^')
+    {
+        sum = sum * sum;
+        cin.ignore();
+    }
+    while (cin >> op >> next)
+    {
+        if (cin.peek() == '^')
+        {
+            next = next * next;
+            cin.ignore();
+        }
+        if (op == ';')
+        {
+            cout << sum << endl;
+            sum  = next;
+        }
+        else if (op == '+')
+            sum = sum + next;
+        else if (op == '-')
+            sum = sum - next;
+    }
+    cout << sum << endl;
+    return 0;
+}
